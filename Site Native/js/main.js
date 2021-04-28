c4c024086a24ddd3bdd0451927b22e46c8402c83
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-04-28 20:33:18 +0300

    Add files via upload

---
+++ Site Native/js/main.js
@@ -0,0 +1,105 @@
+class Main {
+
+    cart = JSON.parse(localStorage.getItem("cart")) || [];
+    id;
+    name;
+    price;
+    image;
+
+    async init() {
+        this.products = await this.loadJSON();
+        this.renderProducts();
+        this.renderCartCount(this.getCartQuantity());
+        this.cartHandler();
+    }
+
+    async loadJSON() {
+        return fetch('product.json').then(response => {
+            return response.json().then(resolved => {
+                return resolved;
+            })
+        });
+    }
+
+    renderProducts() {
+        const container = document.getElementById("cardItems");
+
+        this.products.forEach(product => {
+            container.innerHTML += `
+            <article class="prod">
+            <div class="item-container" data-id="${product.id}">
+                  <div class="card-item">
+                     <img src="img/${product.tag}.png" alt="img" class="card-img">
+                     <p class="item-name">${product.name}</p>
+                     <p class="item-description">${product.description}</p>
+                  </div>
+                  <h3 class="item-price">Price : <span class="item-price-value">${product.price}</span> $ </h3>
+                  <em><button class="product__btn btn add-cart">Add to cart</button></em>
+               </div>
+            </article>`
+        });
+    }
+
+    cartHandler() {
+        const btns = document.querySelectorAll(".add-cart");
+
+        btns.forEach(btn => {
+            btn.addEventListener('click', (event) => {
+                this.addToCart(event);
+            })
+        })
+    }
+
+    addToCart(event) {
+        const element = event.target.parentElement.parentElement;
+        this.id = Number(element.getAttribute("data-id"));
+        this.name = element.querySelector(".item-name").innerHTML;
+        this.price = Number(element.querySelector(".item-price-value").innerHTML);
+        this.image = element.querySelector("img").getAttribute("src");
+
+        if (this.inCart(this.id)) this.modifyQuantity(this.id);
+
+        if (!this.inCart(this.id))
+            this.cart.push(
+                {
+                    id: this.id,
+                    name: this.name,
+                    price: this.price,
+                    image: this.image,
+                    quantity: 1
+                }
+            );
+        localStorage.setItem("cart", JSON.stringify(this.cart));
+        this.renderCartCount(this.getCartQuantity());
+    }
+
+    modifyQuantity(id) {
+        this.cart.map(element => {
+            if (element.id === id) element.quantity++;
+        })
+    }
+
+    inCart() {
+        return this.cart.find(this.searchPredicate, this)
+    }
+
+    searchPredicate(element) {
+        return element.id === this.id;
+    }
+
+    renderCartCount(value) {
+        document.getElementById("cartValue").innerHTML = `${value}`;
+    }
+
+    getCartQuantity() {
+        return this.cart.reduce((accumulator, value) => {
+            return accumulator + value.quantity
+        }, 0)
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const main = new Main();
+    main.init().then(_ => _);
+});
